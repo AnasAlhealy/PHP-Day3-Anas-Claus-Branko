7085ce7424b95338a5d19a36342ffd45e0997c5f
Anas Alhealy <56006995+AnasAlhealy@users.noreply.github.com>
2022-07-13 12:41:09 +0200

    Ex2 using other way foreach loob

--- anas.classwork/classwork2.php
+++ anas.classwork/classwork2.php
@@ -1,4 +1,6 @@
 <?php
+// using two different ways for foreach loop the second one just uncomment (echo "$value . <br>";) done forget to comment the line 21 before executing this .
+$bmw = ["500", "3 Series", "Z4", "X7", "I4", "700P", "550x", "250Q", "750M", "300.sport"];
 $cars = array(
     "bmw" => "500",
     "bmw2019" => "3 Series",
@@ -12,5 +14,9 @@ $cars = array(
     "bmw-pro2010" => "300.sport",
 );
 foreach ($cars as $key => $value) {
-    echo "$value . <br>";
+    // echo "$value . <br>";
 }
+
+foreach ($bmw as $value) {
+    echo "Model: $value <br>";
+};
