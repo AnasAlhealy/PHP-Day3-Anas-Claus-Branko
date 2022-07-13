4c67853c23540d4ada28b4b5a4265c7113751466
Anas Alhealy <56006995+AnasAlhealy@users.noreply.github.com>
2022-07-13 12:32:59 +0200

    Ex2

---
+++ anas.classwork/classwork2.php
@@ -0,0 +1,16 @@
+<?php
+$cars = array(
+    "bmw" => "500",
+    "bmw2019" => "3 Series",
+    "bmw 2020" => "Z4",
+    "bmw2022"   => "X7",
+    "bmw-pro2020" => "I4",
+    "bmw2018" => "700P",
+    "bmw2015" => "550x",
+    "bmw 2012" => "250Q",
+    "bmw2011"   => "750M",
+    "bmw-pro2010" => "300.sport",
+);
+foreach ($cars as $key => $value) {
+    echo "$value . <br>";
+}
